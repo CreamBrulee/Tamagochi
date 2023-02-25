5b1e1e737f9fe8aa046accbd93bc15b5ca7b9f1e
CreamBrulee <mariakon1404@gmail.com>
2023-02-25 15:31:42 +0300

    пояснения к коду commit Maria

--- button_and_consts.py
+++ button_and_consts.py
@@ -5,6 +5,7 @@ import sys
 
 import pygame
 
+# константы и переменные шкал
 FPS = 60
 WIDTH = 800
 HEIGHT = 550
@@ -13,6 +14,7 @@ perc_sleep = 0
 sleeping = 0
 
 
+# функция для загрузки фото
 def load_image(name, colorkey=None):
     if not os.path.isfile(name):
         print(f"Файл с изображением '{name}' не найден")
@@ -26,6 +28,7 @@ def load_image(name, colorkey=None):
     return image
 
 
+# функция заработка денег в играх
 def earning_money(screen, money):
     coin = pygame.transform.scale(load_image('data/coin.png'), (60, 60))
     font = pygame.font.Font(None, 100)
@@ -34,6 +37,7 @@ def earning_money(screen, money):
     intro_rect.topleft = (5, 60)
     screen.blit(coin, (intro_rect.right + 5, 60))
     screen.blit(string_rendered, intro_rect)
+    # функция для загрузки фото сохранение заработанных денег в базу данных
     connect = sqlite3.connect('tamagochi.db')
     cur = connect.cursor()
     cur.execute('UPDATE money SET coins = coins + ?', (money,))
@@ -41,6 +45,7 @@ def earning_money(screen, money):
     connect.close()
 
 
+# класс кнопки
 class Button:
     def __init__(self, x, y, image, sizes, screen, name_for_food_or_for_clothes=None, music=True):
         self.name_for_food_or_for_clothes = name_for_food_or_for_clothes
@@ -55,8 +60,10 @@ class Button:
         self.screen = screen
 
     def draw(self):
+        # функция отрисовки и обработки нажатия кнопки
         action = False
         pos = pygame.mouse.get_pos()
+        # проверка пересечения кнопки и курсова
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] and not self.clicked:
                 self.clicked = True
@@ -65,12 +72,14 @@ class Button:
                     self.click.play()
         if not pygame.mouse.get_pressed()[0]:
             self.clicked = False
-
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
+        # возвращаем была ли нажата кнопка
         return action
 
 
+# функция завершения программы
 def terminate():
+    # запись в бд даты выхода, процентов шкал при выходе, инф. о том, спит ли кот
     connect = sqlite3.connect('tamagochi.db')
     cur = connect.cursor()
     cur.execute('''UPDATE scales SET date = ?,
@@ -78,7 +87,6 @@ def terminate():
     cur.execute('''UPDATE scales SET date = ?,
          percentage = ? WHERE scale = "sleep"''', (datetime.datetime.now(), perc_sleep))
     cur.execute('''UPDATE sleep SET issleeping = ? ''', (sleeping, ))
-    print(sleeping)
     connect.commit()
     connect.close()
     pygame.quit()
